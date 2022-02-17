dd2f38416855c107db70924b3bea973153820e83
Karolina Swislocki <karolinaswislocki@hotmail.com>
2022-02-17 18:29:39 -0500

    install and app httparty to class; destructure params; parsed tags param

--- Gemfile
+++ Gemfile
@@ -12,6 +12,9 @@ gem 'rails', '~> 5.0.1'
 gem 'sqlite3', '~> 1.3.0'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
+# To make HTTP requests to API
+gem 'httparty', '~> 0.20.0'
+
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 # gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,2 +1,16 @@
+
 class Api::PostsController < ApplicationController
+  include HTTParty
+  def posts 
+    tags, sortBy, direction = params.values_at(:tags, :sortBy, :direction)
+    valid_values = ['id', 'reads', 'likes', 'popularity', 'desc', 'asc'];
+    parsed_tags = tags.split(',');
+
+
+
+
+    
+    render status:400, json: {error: "yay"}
+    #render json: {success: parsed_tags} 
+  end 
 end
