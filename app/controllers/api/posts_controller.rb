dbd927bf9d68d86607f7fe15d09323b3ccd419d6
Karolina Swislocki <karolinaswislocki@hotmail.com>
2022-02-17 19:17:57 -0500

    fix sorting bug

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,18 +1,12 @@
 
 class Api::PostsController < ApplicationController
   include HTTParty
-  def posts 
+  def show 
     tags, sortBy, direction = params.values_at(:tags, :sortBy, :direction)
     valid_values = ['id', 'reads', 'likes', 'popularity', 'desc', 'asc'];
     parsed_tags = tags.split(',');
 
-    #if !valid_values.include? sortBy 
-     # return render status:400, json: {error: 'sortBy parameter is invalid'}
-    #end
 
-   # if !valid_values.include? direction
-   #   return render status:400, json: {error: 'direction parameter is invalid'}
-  #  end
 
     if parsed_tags.length == 1
       begin
@@ -23,14 +17,17 @@ class Api::PostsController < ApplicationController
       else
         # if a sortBy value is passed, sort depending on the direction
         if sortBy
-
-          if direction == "asc"
-            posts= posts.sort_by{ |obj| obj[sortBy] }
-          else
+          if direction == "desc"
            posts=posts.sort_by{ |obj| obj[sortBy] }.reverse
+          else
+           posts= posts.sort_by{ |obj| obj[sortBy] }
           end
 
-        return render json: posts
+          return render json: posts
+
+        else
+          return render json: posts
+
         end
       end
 
