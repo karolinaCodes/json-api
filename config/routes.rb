154b99c49fa2a9d27128a670ef4b901738c5e64a
Karolina Swislocki <karolinaswislocki@hotmail.com>
2022-02-17 19:17:45 -0500

    make query parameters optional

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
       get "/ping", to: "ping#ping"
-      get "/posts/:tags/:sortBy/:direction", to: "posts#posts"
+      get "/posts(/:tags(/:sortBy(/:direction)))", to: "posts#show"
   end
 end
