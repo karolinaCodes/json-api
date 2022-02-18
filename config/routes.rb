0ce0ad4c624158e97a78e82a13a014f0ace3258d
Karolina Swislocki <karolinaswislocki@hotmail.com>
2022-02-17 23:28:59 -0500

    add error route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  
   namespace :api do
       get "/ping", to: "ping#ping"
       get "/posts(/:tags(/:sortBy(/:direction)))", to: "posts#show"
   end
+  
+  get '*unmatched_route', to: 'error#not_found'
 end
