95977120226e9007ca78b61839fa0d735e076c41
Karolina Swislocki <karolinaswislocki@hotmail.com>
2022-02-18 19:35:22 -0500

    change api route to include query string

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   
-  namespace :api, defaults: { format: :json } do
+  namespace :api do
       get "/ping", to: "ping#ping"
-      get "/posts(/:tags(/:sortBy(/:direction)))", to: "posts#posts"
+      get "/posts", to: "posts#posts"
   end
   
   get "*unmatched_route", to: "error#not_found"
